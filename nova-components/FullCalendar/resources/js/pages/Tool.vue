20510f203d1857aa60afd56eee746395a2edfab7
Keyzz Rivas <mariorivas1993@live.com>
2023-07-22 00:51:42 -0600

    ✨ Full Calendar

---
+++ nova-components/FullCalendar/resources/js/pages/Tool.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <Head title="Full Calendar" />
+
+    <Heading class="mb-6">
+        Full Calendar
+        <p class="text-sm mt-1">
+            Crear un calendario usando FullCalendar utilizando la tabla de users,
+            <span class="font-mono text-rose-500 rounded">created_at</span> para la fecha en calendario y el nombre de usuario como label
+        </p>
+    </Heading>
+
+    <Card class="min-h-screen p-3">
+        {{  users || 'NO DATA' }}
+        <FullCalendar ref="calendar" :options="calendarOptions" />
+    </Card>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+
+const calendar = ref()
+
+const calendarOptions = {
+    plugins: [
+        dayGridPlugin,
+        timeGridPlugin
+    ],
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    initialView: 'dayGridMonth',
+    timeZone: 'UTC',
+    events: async function (info, successCallback, failureCallback) {
+        try {
+            const params = `start=${info.start.toISOString()}&end=${info.end.toISOString()}`
+            const { data } = await Nova.request().get(`/nova-vendor/full-calendar/users?${params}`)
+            const events = (data.data || []).map((user) => ({
+                id: user.id,
+                title: user.name,
+                start: user.created_at,
+                end: user.created_at,
+            }))
+            successCallback(events)
+        } catch (error) {
+          failureCallback(error)
+        }
+    }
+}
+</script>
---
+++ nova-components/FullCalendar/src/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Findex\FullCalendar\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $request->validate([
+            'start' => ['required', 'string'],
+            'end' => ['required', 'string'],
+        ]);
+
+        $start = Carbon::parse($request->start);
+        $end = Carbon::parse($request->end);
+
+        return response()->json([
+            'data' => User::whereBetween('created_at', [$start, $end])->get()
+        ]);
+    }
+}
